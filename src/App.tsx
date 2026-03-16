acdc23db0b6a830e9cf397017e940519c826370e
wangsikebruno-creator <wangsikebruno@gmail.com>
2026-03-16 11:42:53 +0800

    修复构建错误: 移除未使用的React导入和更新Tailwind CSS配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { StockProvider } from './context/StockContext';
 import HomePage from './pages/HomePage';
